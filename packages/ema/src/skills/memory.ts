e3da3355a91381d4d370e4aeef310aaee80399c9
Myriad-Dreamin <camiyoru@gmail.com>
2025-12-20 23:41:39 +0800

    dev: draft continue

--- packages/ema/src/skills/memory.ts
+++ packages/ema/src/skills/memory.ts
@@ -1,10 +1,53 @@
-interface AgentMemory {
-  search(keywords: string[]): Promise<SearchMemoryResult>;
-  addShortTermMemory(item: ShortTermMemory): Promise<void>;
-  addLongTermMemory(item: LongTermMemory): Promise<void>;
+import type { Message } from "../schema";
+
+/**
+ * Interface for agent memory
+ */
+interface ConversationStorage {
+  /**
+   * Save a conversation
+   * @param messages - The messages in the conversation
+   * @returns Promise resolving to the message ids of the conversation
+   */
+  saveConversation(messages: Message[]): Promise<number[]>;
 }
 
-interface SearchMemoryResult {
+/**
+ * Interface for agent memory
+ */
+interface AgentMemory {
+  /**
+   * Search agent memory
+   * @param keywords - Keywords to search for
+   * @returns Promise resolving to the search result
+   */
+  search(keywords: string[]): Promise<SearchAgentMemoryResult>;
+  /**
+   * Add short term memory
+   * @param item - Short term memory item
+   * @param messageIds - The message ids facilitating the short term memory, for debugging purpose.
+   * @returns Promise resolving when the memory is added
+   */
+  addShortTermMemory(
+    item: ShortTermMemory,
+    messageIds?: number[],
+  ): Promise<void>;
+  /**
+   * Add long term memory
+   * @param item - Long term memory item
+   * @param messageIds - The message ids facilitating the long term memory, for debugging purpose.
+   * @returns Promise resolving when the memory is added
+   */
+  addLongTermMemory(item: LongTermMemory, messageIds?: number[]): Promise<void>;
+}
+
+/**
+ * Result of searching agent memory
+ */
+interface SearchAgentMemoryResult {
+  /**
+   * The long term memories found
+   */
   items: LongTermMemory[];
 }
 
