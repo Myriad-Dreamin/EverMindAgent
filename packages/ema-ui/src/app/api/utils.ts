72aa61db15a94861cd9390a163b5bac489bec321
Myriad-Dreamin <camiyoru@gmail.com>
2025-12-24 18:41:43 +0800

    dev: add docs

--- packages/ema-ui/src/app/api/utils.ts
+++ packages/ema-ui/src/app/api/utils.ts
@@ -2,8 +2,15 @@ import * as k from "arktype";
 
 // https://dev.to/dzakh/zod-v4-17x-slower-and-why-you-should-care-1m1
 // https://arktype.io/
+
+/**
+ * A middleware function to extract the body from the request and validate it against the schema.
+ *
+ * @param extractor - The extractor function to extract the body from the request.
+ * @returns The API route function.
+ */
 const apiData =
-  (extractor = jsonExtractor) =>
+  (extractor: (req: Request) => Promise<any>) =>
   <S>(schema: k.Type<S>) =>
   <F extends (body: S, req: Request) => Promise<Response>>(f: F) =>
   async (req: Request) => {
@@ -17,9 +24,19 @@ const apiData =
     }
     return f(body, req);
   };
-const jsonExtractor = (req: Request) => req.json();
-const queryExtractor = (req: Request) =>
-  Promise.resolve(Object.fromEntries(new URL(req.url).searchParams.entries()));
 
-export const postBody = apiData(jsonExtractor);
-export const getQuery = apiData(queryExtractor);
+/**
+ * A middleware function to extract the body from the request and validate it against the schema.
+ *
+ * @returns The API route function.
+ */
+export const postBody = apiData((req: Request) => req.json());
+
+/**
+ * A middleware function to extract the query parameters from the request and validate it against the schema.
+ *
+ * @returns The API route function.
+ */
+export const getQuery = apiData((req: Request) =>
+  Promise.resolve(Object.fromEntries(new URL(req.url).searchParams.entries())),
+);
